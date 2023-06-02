a145332c46cf008bd1c4f2febfcecd7cd29727b4
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-06-03 00:08:16 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
 
+
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+
+
+};
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', onBtnStartChangeColor);
+refs.btnStop.addEventListener('click', onBtnStopChangeColor);
