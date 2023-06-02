11f31a4d3c9d2aa7247acf9a00f7dbe43a5b78f9
Vasylysaskyba <121983813+Vasylysaskyba@users.noreply.github.com>
2023-06-02 21:28:48 +0000

    Deploying to gh-pages from @ Vasylysaskyba/goit-js-hw-09@15ac25603ff71b081b1c26572a022b6162b596f8 🚀

--- 01-color-switcher.5933fd59.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.btnStop.disabled=!0,t.btnStart.addEventListener("click",onBtnStartChangeColor),t.btnStop.addEventListener("click",onBtnStopChangeColor);
-//# sourceMappingURL=01-color-switcher.5933fd59.js.map
