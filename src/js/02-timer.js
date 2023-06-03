eb4f282ec23d1aef915357c4b998f622630c658b
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-06-03 14:40:55 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,3 +9,70 @@ const daysLabel = document.querySelector('span[data-days]');
 const hoursLabel = document.querySelector('span[data-hours]');
 const minutesLabel = document.querySelector('span[data-minutes]');
 const secondsLabel = document.querySelector('span[data-seconds]');
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please, choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(dataTime, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerContent({ days, hours, minutes, seconds }) {
+  daysLabel.textContent = addLeadingZero(days);
+  hoursLabel.textContent = addLeadingZero(hours);
+  minutesLabel.textContent = addLeadingZero(minutes);
+  secondsLabel.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startBtn.addEventListener('click', updateClock);
+
+function updateClock() {
+  timerId = setInterval(() => {
+    const deltaTime = new Date(dataTime.value) - new Date();
+    startBtn.disabled = true;
+    if (deltaTime >= 0) {
+      const timeObject = convertMs(deltaTime);
+      updateTimerContent(timeObject);
+      dataTime.disabled = true;
+    } else {
+      Notiflix.Notify.success('Countdown finished');
+      clearInterval(timerId);
+      dataTime.disabled = false;
+    }
+  }, 1000);
+}
